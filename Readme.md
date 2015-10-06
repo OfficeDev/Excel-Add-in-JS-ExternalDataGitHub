1c99e4d4d37bf7746276d9568789a56dbe42a539
Uma Subramanian <umas@microsoft.com>
2015-10-06 11:58:50 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,62 @@
+# External Data from GitHub Task Pane Add-in Sample for Excel 2016
+
+_Applies to: Excel 2016_
+
+This task pane add-in shows how to load data from an external service such as by using the GitHub Search APIs in Excel 2016. It comes in two flavors: code editor and Visual Studio.
+
+![External Data GitHub Sample](images/ExternalDataGitHub_data.PNG)
+
+## Try it out
+### Code editor version
+
+The simplest way to deploy and test your add-in is to copy the files to a network share.
+
+1.  Create a folder on a network share (for example, \\\MyShare\ExternalDataGitHub) and copy all the files in the Code Editor folder. 
+2.  Edit the <SourceLocation> element of the manifest file so that it points to the share location from step 1. 
+3.  Copy the manifest (ExternalDataGitHub.xml) to a network share (for example, \\\MyShare\MyManifests).
+4.  Add the share location that contains the manifest as a trusted app catalog in Excel.
+
+    a.  Launch Excel and open a blank spreadsheet.  
+    
+    b.  Choose the **File** tab, and then choose **Options**.
+    
+    c.  Choose **Trust Center**, and then choose the **Trust Center Settings** button.
+    
+    d.  Choose **Trusted App Catalogs**.
+    
+    e.  In the **Catalog Url** box, enter the path to the network share you created in step 3, and then choose **Add Catalog**.
+    
+   f.  Select the **Show in Menu** check box, and then choose **OK**. A message appears to inform you that your settings will be applied the next time you start Office. 
+        
+5.  Test and run the add-in. 
+
+    a.  In the **Insert tab** in Excel 2016, choose **My Add-ins**. 
+    
+    b.  In the **Office Add-ins** dialog box, choose **Shared Folder**.
+    
+    c.  Choose **External Data GitHub Sample**>**Insert**. The add-in opens in a task pane as shown in this diagram. 
+      
+   ![External Data GitHub Sample](images/ExternalDataGitHub_taskpane.PNG) 
+
+    d.  Enter a search keyword and a programming language in the cells, A2 and B2 and click the Get repository info button to load the results into the table in the spreadsheet as shown below.
+    
+      ![External Data GitHub Sample](images/ExternalDataGitHub_data.PNG) 
+    
+### Visual Studio version
+1.  Copy the project to a local folder and open the Excel-Add-in-JS-ExternalDataGitHub.sln in Visual Studio.
+2.  Press F5 to build and deploy the sample add-in. Excel launches and the add-in opens in a task pane to the right of a blank worksheet, as shown in the following figure. 
+        
+  ![External Data GitHub Sample](images/ExternalDataGitHub_taskpane.PNG) 
+
+3.  Enter a search keyword and a programming language in the cells, A2 and B2 and click the Get repository info button to load the results into the table in the spreadsheet as shown below.
+
+  ![External Data GitHub Sample](images/ExternalDataGitHub_data.PNG) 
+
+
+### Learn more
+
+1.  [Excel Add-ins programming overview](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-programming-overview.md)
+2.  [Snippet Explorer for Excel](http://officesnippetexplorer.azurewebsites.net/#/snippets/excel)
+3.  [Excel Add-ins code samples](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-code-samples.md) 
+4.  [Excel Add-ins JavaScript API Reference](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-javascript-reference.md)
+5.  [Build your first Excel Add-in](https://github.com/OfficeDev/office-js-docs/blob/master/excel/build-your-first-excel-add-in.md)
---
+++ Visual Studio Proj/Excel-Add-in-JS-ExternalDataGitHubWeb/App/Home/Home.js
@@ -0,0 +1,171 @@
+﻿/* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+	See full license at the bottom of this file. */
+
+/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    var keyword;
+    var language;
+
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            // Attach a click event handler for the button
+            $('#get-repo-info').click(getRepoInfo);
+
+            // Put the default search keyword and language into the active worksheet
+            // Run a batch operation against the Excel object model
+            Excel.run(function (ctx) {
+
+                var labels = [["Keyword", "Language"],
+                          ["Excel", "JavaScript"]];
+
+                // Create a proxy object for the active sheet
+                var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+
+                // Queue a command to set the value of the range with keyword and language
+                sheet.getRange("A1:B2").values = labels;
+
+                // Queue a command to format the header row
+                sheet.getRange("A1:B1").format.font.bold = true;
+
+                // Queue a command to add a new table to contain the results
+                var table = ctx.workbook.tables.add('A8:G8', true);
+                table.name = "reposTable";
+
+                //Run the queued-up commands, and return a promise to indicate task completion
+                return ctx.sync();
+            })
+            // Always catch errors on the outside of Excel.run()
+            .catch(function (error) {
+                app.showNotification("Error: " + error);
+                console.log("Error: " + error);
+                if (error instanceof OfficeExtension.RuntimeError) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
+        });
+    };
+
+    // Click event handler for the button
+    // Get repo information from GitHub using their public Search API
+    function getRepoInfo() {
+
+        // Run a batch operation against the Excel object model
+        Excel.run(function (ctx) {
+
+            // Create a proxy object for the range that contains the city and state
+            var range = ctx.workbook.worksheets.getActiveWorksheet().getRange("A2:B2");
+
+            // Queue a command to load the values of the range
+            range.load("values");
+
+            //Run the queued-up commands, and return a promise to indicate task completion
+            return ctx.sync().then(function () {
+                // Get the city and state
+                keyword = range.values[0][0];
+                language = range.values[0][1];
+
+                // Create the URL
+                var requestUrl = "https://api.github.com/search/repositories?q=" + keyword + "+language:" + language + "&sort=stars&order=desc";
+
+                // Make the AJAX request to the GitHub Search API
+                // Note that for unauthenticated requests like this, GitHub API allows you to make up to 10 requests per minute.
+                $.ajax({
+                    url: requestUrl,
+                    dataType: "json",
+                    success: function (data) {
+                        // Write the repo info to the active sheet
+                        writeRepoInfo(data);
+                    },
+                    failure: function (error) {
+                        var err = "Error calling GitHUB API: " + error;
+                        app.showNotification(JSON.stringify(err));
+                        console.log(JSON.stringify(err));
+                    }
+                });
+
+            });
+        })
+		.catch(function (error) {
+		    // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+		    app.showNotification("Error: " + error);
+		    console.log("Error: " + error);
+		    if (error instanceof OfficeExtension.Error) {
+		        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+		    }
+		});
+    }
+
+
+    // Write the repo info to the active sheet
+    function writeRepoInfo(repos) {
+
+        // Run a batch operation against the Excel object model
+        Excel.run(function (ctx) {
+
+            // Create a proxy object for the active sheet
+            var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+
+
+            // Create a proxy for the tables rows 
+            var table = ctx.workbook.tables.getItem('reposTable');
+
+
+            // Queue a command to get the newly added table 
+            table.getHeaderRowRange().values = [["NAME", "FULL NAME", "URL", "DESCRIPTION", "FORKS_COUNT", "STAR_GAZERS_COUNT", "WATCHERS_COUNT"]];
+
+
+            // Create a proxy object for the table rows 
+            var tableRows = table.rows;
+            var items = repos.items;
+
+            for (var i in items) {
+                // Queue commands to add some sample rows to the course table 
+                tableRows.add(null, [[items[i].name, items[i].full_name, items[i].url, items[i].description, items[i].forks_count, items[i].stargazers_count, items[i].watchers_count]]);
+            }
+
+            //Run the queued-up commands, and return a promise to indicate task completion
+            return ctx.sync();
+
+        })
+		.catch(function (error) {
+		    // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+		    app.showNotification("Error: " + error);
+		    console.log("Error: " + error);
+		    if (error instanceof OfficeExtension.Error) {
+		        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+		    }
+		});
+    }
+})();
+
+/* 
+Excel-Add-in-JS-ExternalDataGitHub, https://github.com/OfficeDev/Excel-Add-in-JS-ExternalDataGitHub
+
+Copyright (c) Microsoft Corporation
+
+All rights reserved.
+
+MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
